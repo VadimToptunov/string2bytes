6516c42adfe5ae3aa5ab65d50238d1b6f1c414ad
Vadim Toptunov <umnijtony@mail.ru>
2018-01-18 22:37:48 +0300

    Update text_into_shellcode.py

--- text_into_shellcode.py
+++ text_into_shellcode.py
@@ -1,6 +1,6 @@
 def shellcodecreator():
 	inp = raw_input("Enter the payload to convert into bytes here: ")
-	payload = "".join(["\\x%02x" % ord(i) for i in inp])
-	print payload
+	shellcode = "".join(["\\x%02x" % ord(i) for i in inp])
+	print shellcode
 
 shellcodecreator()
